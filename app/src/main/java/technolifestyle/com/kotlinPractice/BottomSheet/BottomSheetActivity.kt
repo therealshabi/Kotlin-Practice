e910eef3b5a6f88b8fb9f354ee69e7f676cb627c
Shahbaz Hussain <shahbaz.h96@gmail.com>
2018-07-13 10:34:05 +0530

    Added Bottom Sheet Activity for displaying a persistent Bottom Sheet

--- app/src/main/java/technolifestyle/com/kotlinPractice/BottomSheet/BottomSheetActivity.kt
+++ app/src/main/java/technolifestyle/com/kotlinPractice/BottomSheet/BottomSheetActivity.kt
@@ -1,13 +1,62 @@
 package technolifestyle.com.kotlinPractice.BottomSheet
 
 import android.os.Bundle
+import android.support.design.widget.BottomSheetBehavior
+import android.support.design.widget.BottomSheetBehavior.STATE_EXPANDED
+import android.support.design.widget.BottomSheetBehavior.STATE_HIDDEN
+import android.support.v4.widget.NestedScrollView
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_bottom_sheet.*
 import technolifestyle.com.kotlinPractice.R
 
+
 class BottomSheetActivity : AppCompatActivity() {
+
+    private lateinit var mBottomSheetBehavior: BottomSheetBehavior<NestedScrollView>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bottom_sheet)
+
+        mBottomSheetBehavior = BottomSheetBehavior.from(nestedScrollView)
+
+        openBottomSheetBtn.setOnClickListener {
+            mBottomSheetBehavior.state = STATE_EXPANDED
+        }
+
+        closeBottomSheetBtn.setOnClickListener{
+            mBottomSheetBehavior.state = STATE_HIDDEN
+        }
+
+        mBottomSheetBehavior.setBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+
+            override fun onStateChanged(view: View, newState: Int) {
+                val state = when (newState) {
+                    BottomSheetBehavior.STATE_DRAGGING -> {
+                        "DRAGGING"
+                    }
+                    BottomSheetBehavior.STATE_SETTLING -> {
+                        "SETTLING"
+                    }
+                    BottomSheetBehavior.STATE_EXPANDED -> {
+                        "EXPANDED"
+                    }
+                    BottomSheetBehavior.STATE_COLLAPSED -> {
+                        "COLLAPSED"
+                    }
+                    BottomSheetBehavior.STATE_HIDDEN -> {
+                        "HIDDEN"
+                    }
+                    else -> ""
+                }
+                Toast.makeText(this@BottomSheetActivity, "Bottom Sheet State Changed to: $state", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+            }
+
+        })
     }
 }
