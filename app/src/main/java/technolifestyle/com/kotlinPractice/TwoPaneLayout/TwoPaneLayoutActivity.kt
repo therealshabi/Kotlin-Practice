d7c7136426a1ed65784fc6c0b88d641d278516ce
Shahbaz Hussain <shahbaz.h96@gmail.com>
2018-07-17 10:55:58 +0530

    Added Readme,Bug Fixes and created two pane layout Activity

---
+++ app/src/main/java/technolifestyle/com/kotlinPractice/TwoPaneLayout/TwoPaneLayoutActivity.kt
@@ -0,0 +1,13 @@
+package technolifestyle.com.kotlinPractice.TwoPaneLayout
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import technolifestyle.com.kotlinPractice.R
+
+class TwoPaneLayoutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_two_pane_layout)
+    }
+}
