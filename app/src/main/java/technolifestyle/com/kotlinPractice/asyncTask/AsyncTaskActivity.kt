83c25c18e2b21947d3895c4cccad38b260b9591d
Shahbaz Hussain <shahbaz.hussain@smartprix.com>
2019-10-26 00:35:42 +0530

    Update async Task UI

--- app/src/main/java/technolifestyle/com/kotlinPractice/asyncTask/AsyncTaskActivity.kt
+++ app/src/main/java/technolifestyle/com/kotlinPractice/asyncTask/AsyncTaskActivity.kt
@@ -4,38 +4,62 @@ import android.os.AsyncTask
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import technolifestyle.com.kotlinPractice.R
+import kotlinx.android.synthetic.main.activity_async_task.*
+import java.lang.ref.WeakReference
 
 class AsyncTaskActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_async_task)
+        setContentView(technolifestyle.com.kotlinPractice.R.layout.activity_async_task)
 
         /*
             AsyncTask executed in Serial by default
             Output would be: C, A, B
          */
-        TestAsyncTask().execute("C", "3000")
-        TestAsyncTask().execute("A", "5000")
-        TestAsyncTask().execute("B", "2000")
+        TestAsyncTask(this).execute("C", "3000")
+        TestAsyncTask(this).execute("A", "5000")
+        TestAsyncTask(this).execute("B", "2000")
 
         /*
             AsyncTask executed in Parallel using THREAD_POOL_EXECUTOR
             Output would be: B, C, A
          */
-        TestAsyncTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, "C", "3000")
-        TestAsyncTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, "A", "5000")
-        TestAsyncTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, "B", "2000")
+//        TestAsyncTask(this).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, "C", "3000")
+//        TestAsyncTask(this).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, "A", "5000")
+//        TestAsyncTask(this).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, "B", "2000")
     }
 
     companion object {
-        class TestAsyncTask : AsyncTask<String, Void, Void>() {
-            override fun doInBackground(vararg params: String?): Void? {
+        var count = 0
+
+        class TestAsyncTask(activity: AsyncTaskActivity) : AsyncTask<String, Void, Pair<String?, String?>>() {
+            private val mWeakActivity: WeakReference<AsyncTaskActivity> = WeakReference(activity)
+
+            override fun doInBackground(vararg params: String?): Pair<String?, String?> {
+                count++
                 val time = params[1]?.toLong() ?: 1
                 Thread.sleep(time)
                 Log.d("Async Task", "Printing numbers: ${params[0]}")
-                return null
+                return Pair(params[0], params[1])
+            }
+
+            override fun onPostExecute(result: Pair<String?, String?>) {
+                super.onPostExecute(result)
+                when (count) {
+                    1 -> {
+                        mWeakActivity.get()?.firstTaskText?.text = result.first
+                        mWeakActivity.get()?.firstTaskTime?.text = "${result.second}ms"
+                    }
+                    2 -> {
+                        mWeakActivity.get()?.secondTaskText?.text = result.first
+                        mWeakActivity.get()?.secondTaskTime?.text = "${result.second}ms"
+                    }
+                    3 -> {
+                        mWeakActivity.get()?.thirdTaskText?.text = result.first
+                        mWeakActivity.get()?.thirdTaskTime?.text = "${result.second}ms"
+                    }
+                }
             }
         }
     }
